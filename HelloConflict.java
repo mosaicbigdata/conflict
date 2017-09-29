80802aa0c43c4215ca3d913c6a3077e129ee7f2d
java <java@bigdata>
2017-09-29 11:13:08 +0900

    while sum

--- HelloConflict.java
+++ HelloConflict.java
@@ -5,8 +5,14 @@ public class HelloConflict {
 
 	public static void main(String[] args) {
 
+		int sum=0;
+		int max = 100;
+		int i=0;
 
-
+		while(max < 100) {
+			i++;
+			sum += i;
+		}	
 
 	}
 }
